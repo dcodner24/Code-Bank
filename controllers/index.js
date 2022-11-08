5532382d8b9822ea91225d27543cf8e39589f024
ChiemekaAnunkor <anunkorjr@gmail.com>
2022-11-08 11:31:04 -0600

    did nothing

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
-const homeRoutes = require("./homeRoutes");
 
+const homeRoutes = require("./homeRoutes");
+const loginRoutes= require("./loginRoutes")
 const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
-
 router.use("/", homeRoutes);
+router.use("/login", loginRoutes)
 
 module.exports = router;
