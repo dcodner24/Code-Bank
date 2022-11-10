09146b0c22ad852812a20bbc65eff66650921bb9
ChiemekaAnunkor <anunkorjr@gmail.com>
2022-11-09 23:58:30 -0600

    finished handle bars routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,16 @@ const router = require("express").Router();
 
 const loginRoutes= require("./loginRoutes")
 const apiRoutes = require("./api");
+const checkingRoutes= require("./checkingRoutes")
 const accountRoutes= require("./accountRoutes")
+const savingsRoutes= require("./savingsRoutes")
+const signupRoutes= require("./signupRoutes")
 
 router.use("/api", apiRoutes);
 router.use("/", loginRoutes);
 router.use("/account", accountRoutes);
+router.use("/checking", checkingRoutes);
+router.use("/savings", savingsRoutes);
+router.use("/signup", signupRoutes);
 
 module.exports = router;
