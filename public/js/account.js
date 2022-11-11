6db533c4609d3265124535b4d93c1c027627cdb0
mtbell412 <mtbell412@gmail.com>
2022-11-11 00:15:06 -0600

    create account js

--- public/js/account.js
+++ public/js/account.js
@@ -1,34 +1,4 @@
-
 console.log("account")
-
-const  transactionForm =async (e)=>{
-e.preventDefault()
-
-const notes = document.querySelector('#t-notes').value.trim();
-const destination = document.querySelector('#t-destination').value.trim();
-const amount = document.querySelector('#t-amount').value.trim();
-
-if (notes && destination && amount) {
-    // Send a POST request to the API endpoint
-    const response = await fetch('/api/transaction', {
-      method: 'POST',
-      body: JSON.stringify({ notes, destination, amount }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-    console.log(response)
-
-  }
-
-  if (response.ok) {
-    // If successful, redirect the browser to the account page
-    document.location.replace('/checking');
-  } else {
-    alert(response.statusText);
-  }
-}
-
-
-// document.querySelector(".transaction-form").addEventListener("submit", transactionForm )
 
 function test(){
   document.querySelector('.logoutb').style.display="block";
@@ -38,14 +8,44 @@ function test(){
 addEventListener('load', test);
 
 
-const checking = async () => {
+
+
+const checking = async (e) => {
+    e.preventDefault();
     document.location.replace('/checking');
 };
 
-const savings = async () => {
+const savings = async (e) => {
+    e.preventDefault()
     document.location.replace('/savings');
 };
 
+const createAccount = async (e) =>{
+  e.preventDefault();
+  const acc_balance = document.querySelector('#deposit').value.trim();
+  let is_checking = document.querySelector('#isChecking').checked;
+  let is_savings = document.querySelector('#isSavings').checked;
 
+  if (deposit) {
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/accounts', {
+      method: 'POST',
+      body: JSON.stringify({ acc_balance, is_checking, is_savings }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+console.log(response)
+    if (response.ok) {
+      // If successful, redirect the browser to the account page
+      document.location.replace('/account');
+    } else {
+      alert(response.statusText);
+    }
+  }
+
+
+}
+
+
+document.querySelector('.create-account').addEventListener('submit',createAccount);
 document.querySelector('#checking').addEventListener('click', checking);
 document.querySelector('#savings').addEventListener('click', savings);
